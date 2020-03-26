75d554df165499e325b59029d6ff32634bf37173
gillettc <cgillett76@hotmail.com>
2020-03-26 11:22:46 +0000

    Lesson 48 Complete

--- SammysAuto/Controllers/ServiceTypesController.cs
+++ SammysAuto/Controllers/ServiceTypesController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using SammysAuto.Data;
 using SammysAuto.Models;
 
@@ -41,6 +42,83 @@ namespace SammysAuto.Controllers
                 return RedirectToAction(nameof(Index));
             }
             return View(serviceType);
+        }
+
+        //GET : ServiceTypes/Details/1
+        public async Task<IActionResult> Details (int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+            var serviceType = await _db.ServiceTypes.SingleOrDefaultAsync(m => m.Id == id);
+            if (serviceType == null)
+            {
+                return NotFound();
+            }
+            return View(serviceType);
+        }
+
+        //GET : ServiceTypes/Edit/1
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+            var serviceType = await _db.ServiceTypes.SingleOrDefaultAsync(m => m.Id == id);
+            if (serviceType == null)
+            {
+                return NotFound();
+            }
+            return View(serviceType);
+        }
+
+        //POST : ServiceType/Edit/1
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, ServiceType serviceType)
+        {
+            if (id != serviceType.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                _db.Update(serviceType);
+                await _db.SaveChangesAsync();
+
+                return RedirectToAction(nameof(Index));
+            }
+            return View(serviceType);
+        }
+
+
+        //GET : ServiceTypes/Delete/1
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+            var serviceType = await _db.ServiceTypes.SingleOrDefaultAsync(m => m.Id == id);
+            if (serviceType == null)
+            {
+                return NotFound();
+            }
+            return View(serviceType);
+        }
+
+        //POST : ServiceType/Delete/1
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> RemoveServiceType(int id)
+        {
+            var serviceType = await _db.ServiceTypes.SingleOrDefaultAsync(m => m.Id == id);
+            _db.ServiceTypes.Remove(serviceType);
+            await _db.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
         }
 
 
