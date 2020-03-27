d0eab14810e355525cabdbab766ccd15a1308ddb
gillettc <cgillett76@hotmail.com>
2020-03-27 14:45:17 +0000

    Lesson 56 Complete

--- SammysAuto/Controllers/UsersController.cs
+++ SammysAuto/Controllers/UsersController.cs
@@ -17,9 +17,33 @@ namespace SammysAuto.Controllers
         }
 
 
-        public IActionResult Index()
+        public IActionResult Index(string option=null, string search=null)
         {
             var users = _db.Users.ToList();
+
+            if (option == "email" && search != null)
+            {
+                users = _db.Users.Where(u => u.Email.ToLower().Contains(search.ToLower())).ToList();
+            }
+            else
+            {
+                if (option == "name" && search != null)
+                {
+                    users = _db.Users.Where(u => u.FirstName.ToLower().Contains(search.ToLower())
+                            || u.LastName.ToLower().Contains(search.ToLower())).ToList();
+                }
+                else
+                {
+                    if (option == "phone" && search != null)
+                    {
+                        users = _db.Users.Where(u => u.PhoneNumber.ToLower().Contains(search.ToLower())).ToList();
+                    }
+
+                }
+
+
+
+            }
             
             return View(users);
         }
